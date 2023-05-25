b172d2b5ac9ebf45cc7523da4aea447f2e18f067
Andres Rodriguez <arodriguezv1@yahoo.com>
2023-05-25 12:59:34 -0500

    Deploying Angular app to gh-pages

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-const ENDPOINT: string = 'https://api.escuelajs.co'
-
-export const environment = {
-  production: true,
-  dashboard: {
-    products: ENDPOINT + '/api/v1/products',
-    categories: ENDPOINT + '/api/v1/categories'
-  }
-};
