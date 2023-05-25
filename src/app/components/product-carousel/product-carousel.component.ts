5093afdec6b2ba66d80012adac1ba3f0bcf64a7c
Andres Rodriguez <arodriguezv1@yahoo.com>
2023-05-25 11:50:59 -0500

    Finish

--- src/app/components/product-carousel/product-carousel.component.ts
+++ src/app/components/product-carousel/product-carousel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/core/services/users.service';
 
 @Component({
   selector: 'app-product-carousel',
@@ -6,36 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-carousel.component.scss']
 })
 export class ProductCarouselComponent implements OnInit {
-
-  products = [
-    { id: 1, name: 'Producto 1', image: 'imagen1.jpg' },
-    { id: 2, name: 'Producto 2', image: 'imagen2.jpg' },
-    { id: 3, name: 'Producto 3', image: 'imagen3.jpg' },
-    // Agrega más productos según sea necesario
-  ];
-
-  slideOffset: number = 0;
-  slideWidth: any = 0;
-  currentIndex: number = 0;
-
-  constructor() { }
+  products: any = [];
+  constructor(
+    private productsService: UsersService,
+    public route: Router
+  ) { }
 
   ngOnInit(): void {
-    this.slideWidth = document.querySelector('.product')?.clientWidth;
+    this.getAllProducts()
   }
 
-  prevSlide() {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-      this.slideOffset += this.slideWidth;
-    }
+  getAllProducts() {
+    this.productsService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
   }
 
-  nextSlide() {
-    if (this.currentIndex < this.products.length - 1) {
-      this.currentIndex++;
-      this.slideOffset -= this.slideWidth;
-    }
+  goToDetails(item: string) {
+    const parametro: any = item;
+    this.route.navigate(['product-details', parametro]);
   }
-
 }
